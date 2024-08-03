06a31df6c732f9701b15e3a126fa2cb71b753210
Dileep <dileepvraj50@gmail.com>
2024-08-03 20:15:14 +0530

    commit message

--- MongoDB_config.sh
+++ MongoDB_config.sh
@@ -6,78 +6,50 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 NORMAL="\e[0m"
 
-ID=$(id -u)
 DATE_TIME=$(date +"%Y-%m-%d %H:%M:%S")
 LOG_FILE=/tmp/mongodb.log
 
-
-
-echo -e "$GREEN Script started executed" &>> $LOG_FILE 
+echo -e "$GREEN Script started executing" &>> $LOG_FILE 
 
 validate_operation() {
-    if [ $1 -ne 0 ]
-        then
-            echo -e "$RED $2 .....failed $NORMAL"
-        else
-            echo -e "$GREEN $2 ....Success $NORMAL"
-            echo -e "$GREEN $2 $NORMAL"
+    if [ $1 -ne 0 ]; then
+        echo -e "$RED $2 .....failed $NORMAL"
+        echo -e "$DATE_TIME: $2 .....failed" &>> $LOG_FILE
+        exit 1
+    else
+        echo -e "$GREEN $2 ....Success $NORMAL"
+        echo -e "$DATE_TIME: $2 ....Success" &>> $LOG_FILE
     fi
 }
 
 validate_user() {
     if [ $ID -ne 0 ]; then
-        echo " $RED can't install services with user normal user please try with user with root permissions $NORMAL"
+        echo -e "$RED Can't install services with a normal user, please try with a user with root permissions $NORMAL"
         exit 1
     else
-        echo " you are root user "
+        echo "You are root user"
     fi
-
 }
 
+# Validate user
+validate_user
+
 # Copying contents of mongo_repo file to /etc/yum.repos.d/ directory.
-
 cp /home/centos/Robo_Shop/repo_files/mongodb_repo /etc/yum.repos.d/mongo.repo &>> $LOG_FILE
-
-if [ $? -eq 0 ]; then
-    echo -e "$GREEN Successfully copied 'mongodb_repo' file to 'yum.repos.d' directory $NORMAL" &>> $LOG_FILE
-else
-    echo -e "$RED 'mongodb_repo' file to 'yum.repos.d' directory is failed $NORMAL" &>> $LOG_FILE
-
-fi
-
-
-# validating copy of 'mongodb_repo' file from project directory to 'yum.repos.d' directory.
 validate_operation $? "Copied mongo_repo file to 'yum.repos.d' directory as mongo.repo"
 
-
-
-# Installing Mongo_db...
-dnf install mongodb-org -y
-
-if [ $? -eq 0 ]; then
-    echo -e "$GREEN Successfully Installed mongo_db $NORMAL" &>> $LOG_FILE
-else
-    echo -e "$RED 'mongodb_db' installation failed $NORMAL" &>> $LOG_FILE
-
-
-# validating 'mongodb' service installation.
+# Installing MongoDB
+dnf install mongodb-org -y &>> $LOG_FILE
 validate_operation $? "Mongo_DB installation"
 
-# enabling mongo_db service.
-systemctl enable mongod
+# Enabling MongoDB service
+systemctl enable mongod &>> $LOG_FILE
 validate_operation $? "Mongo_DB enabled successfully"
 
-# Starting mongo_db service
-systemctl start mongod
-validate_operation $? "successfully started mongodb"
+# Starting MongoDB service
+systemctl start mongod &>> $LOG_FILE
+validate_operation $? "Successfully started MongoDB"
 
-# Now we have to change mongodb default port to public port without manual intervention by using SED
-#.. editor(Stream Editor) where it can edit a text file by reading it in streams.
-
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.conf
-
-validate_operation $? "Successfully modified default port to public port using 'sed' editor."
-
-systemctl restart mongod &>> $LOG_FILE
-
-validate_operation $? "Mongo_db restarted successfully."
+# Modifying MongoDB default port to public port using 'sed'
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOG_FILE
+validate_operation $? "Successfully modified default port
