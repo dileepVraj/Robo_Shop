c065c3dc521bbc42a0d897e5b9b227941e32c311
Dileep <dileepvraj50@gmail.com>
2024-08-03 18:56:59 +0530

    mongodb setup finalversion_1

--- MongoDB_config.sh
+++ MongoDB_config.sh
@@ -17,9 +17,10 @@ echo -e "$GREEN Script started executed" &>> $LOG_FILE
 validate_operation() {
     if [ $1 -ne 0 ]
         then
-            echo -e "$2 $RED .....failed $NORMAL"
+            echo -e "$RED $2 .....failed $NORMAL"
         else
-            echo -e "$2 $GREEN ....Success $NORMAL"
+            echo -e "$GREEN $2 ....Success $NORMAL"
+            echo -e "$GREEN $2 $NORMAL"
     fi
 }
 
@@ -37,7 +38,62 @@ validate_user() {
 
 cp /home/Robo_Shop/repo_files/mongodb_repo /etc/yum.repos.d/mongo.repo &&> $LOG_FILE
 
+if [ $? -eq 0 ]; then
+    echo -e "$GREEN Successfully copied 'mongodb_repo' file to 'yum.repos.d' directory $NORMAL" &>> $LOG_FILE
+else
+    echo -e "$RED 'mongodb_repo' file to 'yum.repos.d' directory is failed $NORMAL" &>> $LOG_FILE
+
+
+# validating copy of 'mongodb_repo' file from project directory to 'yum.repos.d' directory.
 validate_operation $? "Copied mongo_repo file to 'yum.repos.d' directory as mongo.repo"
+
+
+
+# Installing Mongo_db...
+dnf install mongodb-org -y
+
+if [ $? -eq 0 ]; then
+    echo -e "$GREEN Successfully Installed mongo_db $NORMAL" &>> $LOG_FILE
+else
+    echo -e "$RED 'mongodb_db' installation failed $NORMAL" &>> $LOG_FILE
+
+
+# validating 'mongodb' service installation.
+validate_operation $? "Mongo_DB installation"
+
+# enabling mongo_db service.
+systemctl enable mongod
+validate_operation() $? "Mongo_DB enabled successfully"
+
+# Starting mongo_db service
+systemctl start mongod
+validate_operation() $? "successfully started mongodb"
+
+# Now we have to change mongodb default port to public port without manual intervention by using SED
+#.. editor(Stream Editor) where it can edit a text file by reading it in streams.
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongo.conf
+
+validate_operation() $? "Successfully modified default port to public port using 'sed' editor."
+
+systemctl restart mongod &>> $LOG_FILE
+
+validate_operation() $? "Mongo_db restarted successfully."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
