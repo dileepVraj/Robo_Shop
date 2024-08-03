249e689460fb0feef9c8a565bedcc6cf9994e613
dileep50 <dileepvraj50@gmail.com>
2024-08-03 22:24:35 +0530

    mongodb_shell_Script

--- MongoDB_config.sh
+++ MongoDB_config.sh
@@ -1,55 +1,46 @@
 #!/bin/bash
 
 ID=$(id -u)
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-NORMAL="\e[0m"
+LOG_FILE="/tmp/mongodb.log"
 
-DATE_TIME=$(date +"%Y-%m-%d %H:%M:%S")
-LOG_FILE=/tmp/mongodb.log
+# copy mongo.repo file to /etc/yum.repos.d/ 
+cp /home/centos/Robo_Shop/repo_files/mongodb_repo/ /etc/yum.repos.d/mongo.repo
 
-echo -e "$GREEN Script started executing" &>> $LOG_FILE 
+validate_user()
+    if [ $ID -eq 0 ]; then
+        echo "you are root user, good to install services"
+    else
+        echo "only root users can install servies sorry 😒"
+    fi
 
-validate_operation() {
+validate_operation()
     if [ $1 -ne 0 ]; then
-        echo -e "$RED $2 .....failed $NORMAL"
-        echo -e "$DATE_TIME: $2 .....failed" &>> $LOG_FILE
-        exit 1
-    else
-        echo -e "$GREEN $2 ....Success $NORMAL"
-        echo -e "$DATE_TIME: $2 ....Success" &>> $LOG_FILE
+        echo "Sorry $2 failed"
+    elif [ $1 -eq 0 ]; then
+        echo " yeah $2 success"
     fi
-}
 
-validate_user() {
-    if [ $ID -ne 0 ]; then
-        echo -e "$RED Can't install services with a normal user, please try with a user with root permissions $NORMAL"
-        exit 1
-    else
-        echo "You are root user"
-    fi
-}
-
-# Validate user
-validate_user
-
-# Copying contents of mongo_repo file to /etc/yum.repos.d/ directory.
-cp /home/centos/Robo_Shop/repo_files/mongodb_repo /etc/yum.repos.d/mongo.repo &>> $LOG_FILE
-validate_operation $? "Copied mongo_repo file to 'yum.repos.d' directory as mongo.repo"
-
-# Installing MongoDB
+# Installing mongodb
 dnf install mongodb-org -y &>> $LOG_FILE
-validate_operation $? "Mongo_DB installation"
 
-# Enabling MongoDB service
-systemctl enable mongod &>> $LOG_FILE
-validate_operation $? "Mongo_DB enabled successfully"
+validate_operation $? "Installation of Mongo_DB"
 
-# Starting MongoDB service
-systemctl start mongod &>> $LOG_FILE
-validate_operation $? "Successfully started MongoDB"
+# enable mongodb
+systemctl enable mongod
 
-# Modifying MongoDB default port to public port using 'sed'
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOG_FILE
-validate_operation $? "Successfully modified default port"
+validate_operation $? "Mongo_DB enableing is"
+
+# Starting mongodb
+systemctl start mongod
+
+validate_operation $? "Mongo_DB started"
+
+# modify the port number of mongod
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+validate_operation $? "port modification is"
+
+# restarting mongodb
+systemctl restart mongod
+validate_operation $? "mongo_DB restart is success"
+
