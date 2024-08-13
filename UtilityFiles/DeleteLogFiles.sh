01fcf5e516da227d5a471cb165b908db3f2c7f63
Dileep <dileepvraj50@gmail.com>
2024-08-13 21:27:41 +0530

    common commit message on 13-08-2024

--- UtilityFiles/DeleteLogFiles.sh
+++ UtilityFiles/DeleteLogFiles.sh
@@ -65,7 +65,7 @@ FilesToDelete=""
 
 # This function takes 3 arguments (path to directory where files are listed, time(date) and file type)
 listFilesToDelete() {
-    $FilesToDelete=$(find $1 -type f -mtime "+$2" -name "*.$3")
+    $FilesToDelete=$(find $1 -type f -mtime +"$2" -name "*.$3")
 }
 
 listFilesToDelete "/home/shell" "10" "log"
