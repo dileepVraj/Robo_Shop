948fa4eb37321a050ae459964eb73afcf0b58c68
Dileep <dileepvraj50@gmail.com>
2024-09-15 21:34:26 +0530

    commit message *

--- 02_MongoDB.sh
+++ 02_MongoDB.sh
@@ -14,19 +14,17 @@ Mongo_Installation=$(command -v mongod)
 
 validate_user() {
     if [ "$UID" -eq 0 ]; then
-        echo -e "$GREEN""You are root user, good to install services.""$WHITE"
-        
-        
-
+    echo -e "$GREEN""You are root user, good to install services.""$WHITE"
     else
         echo -e "$RED Only root users can install services, sorry 😒 $WHITE "
         
-        read -pr "Do you want to switch as root user? (yes/no): " response
+        echo "$CYAN Do you want to switch as root user? (yes/no):$WHITE"
+        read response 
         if [ "$response" = "yes" ]; then
             sudo su -
             validate_operation $Exit_Status "Switch to root user"
         else
-            echo "You have choose to not switch as root user, we can't install packages, hence exiting"
+            echo "$CYAN You have choose to not switch as root user, we can't install packages, hence exiting$WHITE"
             exit
         fi
     fi
@@ -49,12 +47,13 @@ check_and_install_gnupg(){
     # and communications. GnuPG allows users to encrypt data and create digital signatures to ensure 
     # the integrity and confidentiality of files, emails, and other forms of digital communication.
 
+    # Checking is gnupg is installed.
     if command -v gpg &> /dev/null;then
     echo "$BLUE gnupg already installed on your machine$WHITE"
     else
-        echo "$RED gnupg is not installed, Installing.... $WHITE"
+        echo "$CYAN gnupg is not installed.$WHITE"
         echo "$YELLOW installing gnupg...$WHITE"
-        apt install gnupg -y
+        apt install gnupg -y &> /dev/null
         echo "$GREEN gnupg installed successfully $WHITE"
     fi
 
@@ -101,10 +100,11 @@ create_list_file() {
     validate_operation $Exit_Status "MongoDB source list file has been created at /etc/apt/sources.list.d/mongodb-org-7.0.list is"
 }
 
-reload_package_database(){
+
+reload_package_manager(){
     echo "$YELLOW updating apt packages...$WHITE"
     apt update -y &> /dev/null
-    validate_operation $Exit_Status "Package Update"
+    validate_operation $Exit_Status "APT package Update"
 
 }
 
@@ -116,32 +116,32 @@ install_mongo_db_community_server(){
         validate_user
         echo "$YELLOW installing mongodb...$WHITE"
         apt install mongodb-org -y &> $LOG_FILE
-        validate_operation $Exit_Status "MongoDB installation is"
+        validate_operation $Exit_Status "MongoDB installation"
 
         if command -v mongod;then
             systemctl enable mongod
-            validate_operation $Exit_Status "Enabling MongoDB is"
+            validate_operation $Exit_Status "Enabling MongoDB"
             systemctl start mongod
-            validate_operation $Exit_Status "Mongodb start is"
+            validate_operation $Exit_Status "Starting MongoDB"
         fi
     fi
 }
 
 modify_mongodb_selfBind_IP(){
     sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-    validate_operation $Exit_Status "MongoDB Self_Bind modification is"
+    validate_operation $Exit_Status "MongoDB Self_Bind modification"
 }
 
 restart_mongoDB(){
     # restarting mongodb
     systemctl restart mongod
-    validate_operation $? "mongo_DB restart is"
+    validate_operation $? "Restarting MongoDB"
 }
 
 check_and_install_gnupg
 import_mongo_db_gpg_key
 create_list_file
-reload_package_database
+reload_package_manager
 install_mongo_db_community_server
 modify_mongodb_selfBind_IP
 restart_mongoDB
