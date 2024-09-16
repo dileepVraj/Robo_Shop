ebb0b6b9b208cd7f5efaf27d84e287534e60241c
Dileep <dileepvraj50@gmail.com>
2024-09-16 09:48:11 +0530

    commit of 16-09-2024

--- 08_Shipping.sh
+++ 08_Shipping.sh
@@ -1,124 +1,193 @@
 #!/bin/bash
+ID=$(id -u)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+WHITE=$(tput setaf 7)
+CYAN=$(tput setaf 6)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
 
-LOG_FILE="/tmp/service.log"
-
-validateUser() {
-    if [ $(id -u) -eq 0 ] ; then
-    echo " You are root user good to go"
+validateOperation(){
+    if [ $1 -eq 0 ];then
+    echo "$GREEN $2 is successful$WHITE"
     else
-        echo "You are not a root user, wait switching to root user"
-        sudo su -
-        echo " Switched to root user"
+        echo "$RED $2 is fail$WHITE"
     fi
 }
 
-validateOperation() {
-    if [ $1 -eq 0 ]; then
-    echo "'$2' is successfull"
-    else
-        echo "'$2' is falied"
-        exit 1
-    fi
+updateAptPackage(){
+    #Updating packages.
+    echo "$CYAN updating apt packages $WHITE"
+    apt update -y &> /dev/null
+    validateOperation $? "Updating apt packages"
+    
 }
 
-addUser() {
-    if id "$1" &>/dev/null;then
-    echo "Yes user "$1" exists."
+add_user() {
+    # This function takes one positional parameter(username).
+    # The 'id $1(positional parameter 1) returns response of user details if present, if not an error...
+    # ..response along with the exit status.'
+
+    # If 'id $1' fails the error response is supress to /dev/null directory where it will deleted input
+    # automatically.
+
+    if id "$1" &>/dev/null; then
+        echo "$BLUE Yes user '$1' exists.$WHITE"
     else
-        echo "No user "$1" is not available."
+        echo "$RED No user '$1' is not available.$WHITE"
+        echo "$BLUE creating $1 user$WHITE"
         useradd "$1"
-        echo "New user "$1" is created."
+        echo "$GREEN User '$1' is created.$WHITE"
     fi
-
 }
 
 createDirectory() {
-    if [ -d $1 ]; then
-    echo "Directory '$1' is available."
+    # This function takes 1 arguments, 1.path to directory we want create.
+
+    # Usage of this function:
+    # This function checks for a directory is present or not which passed as argument.
+    # If available it will prints directory is available message.
+    # If directory is not available it will creates directory and prints the success message.
+    
+    # The '-d' option in bash is used within conditional expressions to test wheather a path is a directory.
+
+    # The '-d' is typically used with 'test' command or within a '[' which is a synonym for 'test'.
+    # It checks if given path exists and is a directory.
+
+
+    if [ -d $1  ]; then
+        echo "$GREEN directory $1 is available $WHITE"
     else
-        echo "Directory '$1' is not available"
-        mkdir -p "$1"
-        echo "Directory '$1' is created successfully."
-    fi  
+        echo "$CYAN directory $1 is not available $WHITE"
+        # The option '-p' is used to create the parent directory if not exists,which specified in path.
+        # Ex: mkdir -p /home/dileep/games this command creates 'games' directory in /home/dileep directory,
+        # If /home/dileep is not present it will creates /home/dileep directory without failing the command.
+        echo "$YELLOW creating directory $WHITE"
+        mkdir -p $1
+        echo "$GREEN directory /app created successfully.$WHITE"
+        
+    fi
+}   
+
+installMaven(){
+    if command -v mvn &> /dev/null;then
+    echo "Maven is installed"
+    echo "Maven version:"
+    mvn -version
+    return 0
+    else
+        echo "Maven is not installed or not in path"
+        echo "Installing maven..."
+        apt install maven -y
+        validateOperation $? "Installing maven"
+    fi
+
+
 }
 
-# Shipping service is responsible for finding the distance of the package to be shipped and calculate the price based on that.
+downloadingApplicationCode(){
+    # Downloading 'shipping' application code to /tmp directory.
+    curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &> /dev/null
+    validate_operation $? "Downloading application code"
 
-# Shipping service is written in Java, Hence we need to install Java.
+}
 
-# Maven is a Java Packaging software, Hence we are going to install maven, This indeed takes care of java installation.
+unzipTheApplicationCode(){
+    # This function is to unzip application code in /app directory , we using option '-o'
+    # ....to override if any same files exists in the directory.
 
-# ==================================================================================================#
+    if ! command -v unzip; then
+    echo "$RED unzip isn't installed do you want to install yes/no $WHITE"
+    echo "$YELLOW installing unzip utility... $WHITE"
+    apt install unzip -y &> /dev/null
+    validate_operation $? "unzip utility installation is"
+    else
+        echo "unzip utility already installed"
+        
+    fi
 
-# Installing Maven.
-dnf install maven -y
+    # changing to directory after creation if 'cd' command faile because no directory exists then program will
+    # exists.
+    # The '||' OR operator is used to execute the command following it only if preceding command fails(returns a non-zero exit status).
+    cd "/app" || exit
+    echo "$BLUE changed to directory /app $WHITE" 
 
-# Configure the application, adding user.
-addUser "roboshop"
+    # unziping the downloaded catalogue.zip file in /app directory.
+    unzip -oq unzip /tmp/shipping.zip
+    validate_operation $? "Unziping application code in 'tmp/shipping.zip is'"
 
-# Crating application directory.
+}
+
+cleanMavenPackage(){
+    echo "Cleaning maven package..."
+    mvn clean package &> /dev/null
+
+    
+
+}
+
+renameShippingJar(){
+
+    mv target/shipping-1.0.jar shipping.jar
+    echo "Renamed shippingJar"
+}
+
+creatingServiceFile(){
+    # adding shipping.service file in /etc/systemd/system/ directory.
+    cp /home/Robo_Shop/service_files/Shipping_service /etc/systemd/system/shipping.service
+    validate_operation $? "shipping.service is created"
+}
+
+daemonReload(){
+    systemctl daemon-reload
+    echo "Daemon reloaded successfully"
+}
+
+enableAndStartShippingService(){
+    systemctl enable shipping 
+    systemctl start shipping
+}
+
+InstallMySqlClient(){
+    echo "Updateing packages"
+    apt update -y &> /dev/null
+
+    echo "installing mysql client"
+    apt install mysql-client -y &> /dev/null
+
+}
+
+loadSchemaToMySqlServer(){
+    echo "Loading schema of shipping addresses to mysql database"
+    mysql -h $1 -uroot -pdileep1# < /app/schema/shipping.sql 
+    echo "Successfully loaded schema to mysql-server"
+
+    echo "Restarting shipping"
+    systemctl restart shipping
+
+
+}
+
+updateAptPackage
+add_user "roboshop"
 createDirectory "/app"
-
-# Changing to '/app' directory.
-cd /app
-validateOperation $? "Changed to directory /app"
-
-# Downloading application code to /tmp directory.
-curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
-validateOperation $? "Appliacation Download"
-
-# unziping application code in /app directory.
-unzip -o /tmp/shipping.zip
-validateOperation $? "Application code unziped"
-
-# Every application is developed by development team will have some common softwares that they use as libraries. 
-# This application also have the same way of defined dependencies in the application configuration.
-#--------------------------------------------
+installMaven
+downloadingApplicationCode
+unzipTheApplicationCode
+cleanMavenPackage
+renameShippingJar
+daemonReload
+enableAndStartShippingService
+InstallMySqlClient
+loadSchemaToMySqlServer "172.31.36.197"
 
 
-# installing maven package.
-mvn clean package &&> $LOG_FILE
-validateOperation $? "Maven package installation"
 
-# Re-naming 'shipping-1.0.jar' to shipping.jar
-mv target/shipping-1.0.jar shipping.jar
-validateOperation $? "Renamed shipping-1.0 to shipping.jar"
 
-# We need to setup a new service in systemd so systemctl can manage this service
-#--------------------------------------------
 
-# Copying shipping_servie to /tmp/systemd/system directory
-cp /home/Robo_Shop/service_files/Shipping_service /etc/systemd/system/shipping.service
-validateOperation $? "creating shipping.service file"
 
-# Daemon-reload
-systemctl daemon-reload
-validateOperation $? "Dameon reload"
 
-# enabling shipping.service
-systemctl enable shipping
-validateOperation $? "Enabling shipping.service"
 
-# Starting shipping.service
-systemctl start shipping
-validateOperation $? "Starting shipping.service"
-
-# For this application to work fully functional we need to load schema to the Database.
-# We need to load the schema. To load schema we need to install mysql client.
-# To have it installed we can use.
-#-----------------------------------
-
-# Installing mysql client.
-dnf install mysql -y
-validateOperation $? "Mysql client install"
-
-# Loading shipping schema to mysql database.
-mysql -h 172.31.32.21 -uroot -pRoboShop@1 < /app/schema/shipping.sql
-validateOperation $? "Successfully loaded schema to MySql database"
-
-# Restarting shipping.service
-systemctl restart shipping
-validateOperation $? "Restart shipping.service"
 
 
 
